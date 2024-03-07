2ef191f2734fb2b9cefc08f6dc553e7f8b30aa3c
unknown <charan3880@gmail.com>
2024-03-07 18:13:47 +0530

    changes

--- 01_hello_world.py
+++ 01_hello_world.py
@@ -1,1 +1,9 @@
+#------------------------------------------------------------------------
+#
+# This is a Python program to print 'Hello, World!' to user.
+#
+# AUTHOR  : charan tej
+# DATE    : 7th March, 2024
+# VERSION : 1.0
+#------------------------------------------------------------------------
 print('Hello,World!')
